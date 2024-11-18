6758b65916c5ca301adb583fd76e96c228eb82e9
Sylar <swlfigo@gmail.com>
2024-11-18 21:40:02 +0800

    Update EventBus

--- lib/component/eventbus/eventbus.dart
+++ lib/component/eventbus/eventbus.dart
@@ -1,44 +1,26 @@
-//订阅者回调签名
-typedef void EventCallback(arg);
+import 'dart:async';
 
 class EventBus {
-  //私有构造函数
+  static final EventBus _instance = EventBus._internal();
+
+  factory EventBus() => _instance;
+
   EventBus._internal();
 
-  //保存单例
-  static EventBus _singleton = EventBus._internal();
+  final _eventController = StreamController<dynamic>.broadcast();
 
-  //工厂构造函数
-  factory EventBus() => _singleton;
-
-  //保存事件订阅者队列，key:事件名(id)，value: 对应事件的订阅者队列
-  final _emap = Map<Object, List<EventCallback>?>();
-
-  //添加订阅者
-  void on(eventName, EventCallback f) {
-    _emap[eventName] ??= <EventCallback>[];
-    _emap[eventName]!.add(f);
+  void fire(dynamic event) {
+    _eventController.add(event);
   }
 
-  //移除订阅者
-  void off(eventName, [EventCallback? f]) {
-    var list = _emap[eventName];
-    if (eventName == null || list == null) return;
-    if (f == null) {
-      _emap[eventName] = null;
-    } else {
-      list.remove(f);
-    }
+  StreamSubscription on<T>(void Function(T event) onData) {
+    return _eventController.stream
+        .where((event) => event is T)
+        .cast<T>()
+        .listen(onData);
   }
 
-  //触发事件，事件触发后该事件所有订阅者会被调用
-  void emit(eventName, [arg]) {
-    var list = _emap[eventName];
-    if (list == null) return;
-    int len = list.length - 1;
-    //反向遍历，防止订阅者在回调中移除自身带来的下标错位
-    for (var i = len; i > -1; --i) {
-      list[i](arg);
-    }
+  void dispose() {
+    _eventController.close();
   }
 }
